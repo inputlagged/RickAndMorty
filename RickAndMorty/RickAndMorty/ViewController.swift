578e2a04a7a06759fb815bc3818743ba54fc1464
inputlagged <pavelseidhe@gmail.com>
2022-06-04 11:14:49 +0600

    Initial

---
+++ RickAndMorty/RickAndMorty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Pavel Epaneshnikov on 4/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
