42125ab9561cd542cfd6b4a01c7f3dbba48a62ba
inputlagged <pavelseidhe@gmail.com>
2022-06-04 12:32:12 +0600

    TableView setup[

---
+++ RickAndMorty/RickAndMorty/Screen/Character/List/Cell/CharacterTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CharacterTableViewCell.swift
+//  RickAndMorty
+//
+//  Created by Pavel Epaneshnikov on 4/6/22.
+//
+
+import UIKit
+
+class CharacterTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var characterImageView: UIImageView!
+    @IBOutlet weak var characterNameLabel: UILabel!
+}
