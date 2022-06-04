d52ca54018aa76b17a39c5ee95c0421faee8ce59
inputlagged <pavelseidhe@gmail.com>
2022-06-04 12:57:44 +0600

    Character cell

--- RickAndMorty/RickAndMorty/Screen/Character/List/Cell/CharacterTableViewCell.swift
+++ RickAndMorty/RickAndMorty/Screen/Character/List/Cell/CharacterTableViewCell.swift
@@ -11,4 +11,17 @@ class CharacterTableViewCell: UITableViewCell {
 
     @IBOutlet weak var characterImageView: UIImageView!
     @IBOutlet weak var characterNameLabel: UILabel!
+    private var avatarLoadTask: URLSessionTask?
+    
+    func setup(character: Character) {
+        self.characterNameLabel.text = character.name
+        guard let imageURL = URL(string: character.image) else { return }
+        avatarLoadTask = URLSession.shared.dataTask(with: imageURL, completionHandler: { data, _, _ in
+            guard let data = data, let image = UIImage(data: data) else { return }
+            DispatchQueue.main.async {
+                self.characterImageView.image = image
+            }
+        })
+        avatarLoadTask?.resume()
+    }
 }
